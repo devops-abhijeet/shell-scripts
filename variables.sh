3b8bef742999916ab893de91656ac7a647094f9e
Dev-Abhijeet <abhijeetkamble14092000@gmail.com>
2025-05-03 08:51:14 +0530

    New Shell Scripts

--- variables.sh
+++ variables.sh
@@ -5,7 +5,30 @@
 ## System Variable
 echo $USER
 echo $LANG
-## User Defined Variable
 
+## User Defined Variable
 username="Abhi"
 echo "My Name is : $username"
+
+## Unsetting Variables
+
+var1="newdata"
+var2=123
+
+echo $var1 $var2
+
+unset $var1
+
+echo $var1 $var2
+
+## Read Only Variable
+
+v1=1234
+
+readonly v1
+
+echo $v1
+
+v1=2
+
+echo $v1
