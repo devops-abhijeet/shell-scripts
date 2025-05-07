1c7a184dd24fb357c5b78cb7d425efd81d68ccf1
Dev-Abhijeet <abhijeetkamble14092000@gmail.com>
2025-05-07 12:35:20 +0530

    added some more examples

--- for_loop_examples.sh
+++ for_loop_examples.sh
@@ -4,3 +4,26 @@
 # Author : Abhijeet K
 # Reason : Examples of For loops
 
+#Example 1
+var=(apple mango cherii)
+
+for var1 in "${var[@]}"
+do
+	echo "The fruit is : $var1"
+
+done
+
+echo
+echo "-------------------"
+echo
+
+#Example 2
+for i in 1 2 3 4 5 6
+do
+	if [ $(( i%2 )) -eq 0 ];then
+		echo "THe number is Even : $i"
+	fi
+	
+done
+
+
