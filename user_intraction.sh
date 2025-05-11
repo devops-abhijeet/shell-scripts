bc75d04698edc8f9ef0135dfa9ce3e87100114db
Dev-Abhijeet <abhijeetkamble14092000@gmail.com>
2025-05-11 23:03:02 +0530

    User interaction in Shell script

---
+++ user_intraction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Author : Abhijeet
+
+# Ask User to enter value
+
+echo "Enter Name :"
+read name
+echo "Your name is : $name"
+
+# Take input using read command only
+
+read -p "Enter your age : " age
+echo "Your age is : $age"
